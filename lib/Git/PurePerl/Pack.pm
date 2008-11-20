e8243d004353c227f70230ce659c3f29274cfd51
Léon Brocard <acme@astray.com>
2008-11-20 09:31:54 +0000

    Import of LBROCARD/Git-PurePerl-0.33 from CPAN.
    gitpan-cpan-distribution: Git-PurePerl
    gitpan-cpan-version:      0.33
    gitpan-cpan-path:         LBROCARD/Git-PurePerl-0.33.tar.gz
    gitpan-cpan-author:       LBROCARD
    gitpan-cpan-maturity:     released

--- lib/Git/PurePerl/Pack.pm
+++ lib/Git/PurePerl/Pack.pm
@@ -8,7 +8,8 @@ use IO::File;
 has 'filename' =>
     ( is => 'ro', isa => 'Path::Class::File', required => 1, coerce => 1 );
 
-has 'version' => ( is => 'rw', isa => 'Int', required => 0 );
+has 'version'       => ( is => 'rw', isa => 'Int', required => 0 );
+has 'global_offset' => ( is => 'rw', isa => 'Int', required => 0 );
 
 has 'offsets' =>
     ( is => 'rw', isa => 'ArrayRef[Int]', required => 0, auto_deref => 1, );
@@ -39,6 +40,7 @@ sub BUILD {
     my $fh   = $self->open_index;
     $fh->read( my $signature, 4 );
     $fh->read( my $version,   4 );
+    $version = unpack( 'N', $version );
 
     if ( $signature eq "\377tOc" ) {
         confess("Unknown version") if $version != 2;
@@ -46,20 +48,28 @@ sub BUILD {
         $version = 1;
     }
     $self->version($version);
+
+    if ( $version == 1 ) {
+        $self->global_offset(0);
+    } else {
+        $self->global_offset(8);
+    }
+
     my @offsets = (0);
     foreach my $i ( 0 .. $FanOutCount - 1 ) {
-        $fh->seek( $i * $IdxOffsetSize, 0 );
+        $fh->seek( ( $i * $IdxOffsetSize ) + $self->global_offset, 0 );
         $fh->read( my $data, $IdxOffsetSize );
         my $offset = unpack( 'N', $data );
         confess("pack has discontinuous index") if $offset < $offsets[-1];
         push @offsets, $offset;
     }
+
     $self->size( $offsets[-1] );
 }
 
 sub open_index {
-    my $self     = shift;
-    my $filename = $self->filename;
+    my $self           = shift;
+    my $filename       = $self->filename;
     my $index_filename = $filename;
     $index_filename =~ s/\.pack/.idx/;
     my $fh = IO::File->new($index_filename) || confess($!);
@@ -75,25 +85,62 @@ sub open_pack {
 
 sub get_object {
     my ( $self, $want_sha1 ) = @_;
-    my $fh  = $self->open_index;
-    my $pos = $OffsetStart;
-    foreach my $i ( 1 .. $self->size ) {
-        $fh->seek( $pos, 0 ) || die $!;
-        $fh->read( my $data, $OffsetSize ) || die $!;
-        my $offset = unpack( 'N', $data );
-        $fh->read( $data, $SHA1Size ) || die $!;
-        my $sha1 = unpack( 'H*', $data );
-        if ( $sha1 eq $want_sha1 ) {
-            return $self->unpack_object($offset);
+
+    if ( $self->version == 1 ) {
+        my $fh  = $self->open_index;
+        my $pos = $OffsetStart;
+        foreach my $i ( 1 .. $self->size ) {
+            $fh->seek( $pos, 0 ) || die $!;
+            $fh->read( my $data, $OffsetSize ) || die $!;
+            my $offset = unpack( 'N', $data );
+            $fh->read( $data, $SHA1Size ) || die $!;
+            my $sha1 = unpack( 'H*', $data );
+            if ( $sha1 eq $want_sha1 ) {
+                return $self->unpack_object($offset);
+            }
+            $pos += $EntrySize;
         }
-        $pos += $EntrySize;
+        return;
+
+    } else {
+
+        my $fh = $self->open_index;
+        my @data;
+        my $pos = $OffsetStart;
+        foreach my $i ( 0 .. $self->size - 1 ) {
+            $fh->seek( $pos + $self->global_offset, 0 ) || die $!;
+            $fh->read( my $sha1, $SHA1Size ) || die $!;
+            $data[$i] = [ unpack( 'H*', $sha1 ), 0, 0 ];
+            $pos += $SHA1Size;
+        }
+        foreach my $i ( 0 .. $self->size - 1 ) {
+            $fh->seek( $pos + $self->global_offset, 0 ) || die $!;
+            $fh->read( my $crc, $CrcSize ) || die $!;
+            $data[$i]->[1] = unpack( 'H*', $crc );
+            $pos += $CrcSize;
+        }
+        foreach my $i ( 0 .. $self->size - 1 ) {
+            $fh->seek( $pos + $self->global_offset, 0 ) || die $!;
+            $fh->read( my $offset, $OffsetSize ) || die $!;
+            $data[$i]->[2] = unpack( 'N', $offset );
+            $pos += $OffsetSize;
+        }
+
+        foreach my $data (@data) {
+            my ( $sha1, $crc, $offset ) = @$data;
+            if ( $sha1 eq $want_sha1 ) {
+                return $self->unpack_object($offset);
+            }
+        }
+        return;
+
     }
-    return;
 }
 
 sub unpack_object {
     my ( $self, $offset ) = @_;
-    my $fh = $self->open_pack;
+    my $obj_offset = $offset;
+    my $fh         = $self->open_pack;
 
     $fh->seek( $offset, 0 ) || die $!;
 
@@ -117,9 +164,10 @@ sub unpack_object {
     $type = $TYPES[$type];
 
     if ( $type eq 'ofs_delta' || $type eq 'ref_delta' ) {
-        die "deltified";
-
-#            data, type = unpack_deltified(packfile, type, offset, obj_offset, size, options)
+        ( $type, $size, my $content )
+            = $self->unpack_deltified( $fh, $type, $offset, $obj_offset,
+            $size );
+        return ( $type, $size, $content );
 
     } elsif ( $type eq 'commit'
         || $type eq 'tree'
@@ -150,4 +198,109 @@ sub read_compressed {
     return $out;
 }
 
+sub unpack_deltified {
+    my ( $self, $fh, $type, $offset, $obj_offset, $size ) = @_;
+
+    my $base;
+
+    $fh->seek( $offset, 0 ) || die $!;
+    $fh->read( my $data, $SHA1Size ) || die $!;
+    my $sha1 = unpack( 'H*', $data );
+
+    if ( $type eq 'ofs_delta' ) {
+        my $i           = 0;
+        my $c           = unpack( 'C', substr( $data, $i, 1 ) );
+        my $base_offset = $c & 0x7f;
+
+        while ( ( $c & 0x80 ) != 0 ) {
+            $c = unpack( 'C', substr( $data, ++$i, 1 ) );
+            $base_offset++;
+            $base_offset <<= 7;
+            $base_offset |= $c & 0x7f;
+        }
+        $base_offset = $obj_offset - $base_offset;
+        $offset += $i + 1;
+
+        ( $type, undef, $base ) = $self->unpack_object($base_offset);
+    } else {
+        ( $type, undef, $base ) = $self->get_object($sha1);
+        $offset += $SHA1Size;
+
+    }
+
+    my $delta = $self->read_compressed( $fh, $offset, $size );
+    my $new = $self->patch_delta( $base, $delta );
+
+    return ( $type, length($new), $new );
+}
+
+sub patch_delta {
+    my ( $self, $base, $delta ) = @_;
+    my ( $src_size, $pos ) = $self->patch_delta_header_size( $delta, 0 );
+    if ( $src_size != length($base) ) {
+        confess "invalid delta data";
+    }
+
+    ( my $dest_size, $pos ) = $self->patch_delta_header_size( $delta, $pos );
+    my $dest = "";
+
+    while ( $pos < length($delta) ) {
+        my $c = substr( $delta, $pos, 1 );
+        $c = unpack( 'C', $c );
+        $pos++;
+        if ( ( $c & 0x80 ) != 0 ) {
+
+            my $cp_off  = 0;
+            my $cp_size = 0;
+            $cp_off = unpack( 'C', substr( $delta, $pos++, 1 ) )
+                if ( $c & 0x01 ) != 0;
+            $cp_off |= unpack( 'C', substr( $delta, $pos++, 1 ) ) << 8
+                if ( $c & 0x02 ) != 0;
+            $cp_off |= unpack( 'C', substr( $delta, $pos++, 1 ) ) << 16
+                if ( $c & 0x04 ) != 0;
+            $cp_off |= unpack( 'C', substr( $delta, $pos++, 1 ) ) << 24
+                if ( $c & 0x08 ) != 0;
+            $cp_size = unpack( 'C', substr( $delta, $pos++, 1 ) )
+                if ( $c & 0x10 ) != 0;
+            $cp_size |= unpack( 'C', substr( $delta, $pos++, 1 ) ) << 8
+                if ( $c & 0x20 ) != 0;
+            $cp_size |= unpack( 'C', substr( $delta, $pos++, 1 ) ) << 16
+                if ( $c & 0x40 ) != 0;
+            $cp_size = 0x10000 if $cp_size == 0;
+
+            $dest .= substr( $base, $cp_off, $cp_size );
+        } elsif ( $c != 0 ) {
+            $dest .= substr( $delta, $pos, $c );
+            $pos += $c;
+        } else {
+            confess 'invalid delta data';
+        }
+    }
+
+    if ( length($dest) != $dest_size ) {
+        confess 'invalid delta data';
+    }
+    return $dest;
+}
+
+sub patch_delta_header_size {
+    my ( $self, $delta, $pos ) = @_;
+
+    my $size  = 0;
+    my $shift = 0;
+    while (1) {
+
+        my $c = substr( $delta, $pos, 1 );
+        unless ( defined $c ) {
+            confess 'invalid delta header';
+        }
+        $c = unpack( 'C', $c );
+
+        $pos++;
+        $size |= ( $c & 0x7f ) << $shift;
+        $shift += 7;
+        last if ( $c & 0x80 ) == 0;
+    }
+    return ( $size, $pos );
+}
 1;
--- t/simple.t
+++ t/simple.t
@@ -1,24 +1,26 @@
 #!perl
 use strict;
 use warnings;
-use Test::More tests => 34;
+use Test::More tests => 153;
 use Git::PurePerl;
 
-foreach my $directory qw(test-project test-project-packs) {
+foreach my $directory qw(test-project test-project-packs test-project-packs2)
+{
     my $git = Git::PurePerl->new( directory => $directory );
-    my $master = $git->master;
-    is( $master->kind,   'commit' );
-    is( $master->size,   256 );
-    is( $master->sha1,   'd60f7006a71288b70601fa6057fd3727842a74a0' );
-    is( $master->tree,   '37b4fcd62571f07408e830f455268891f95cecf5' );
-    is( $master->parent, 'bb99a61fd2035d672d075ea4b72d17ac0d1c193e' );
-    like( $master->author,
-        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
-    like( $master->committer,
-        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
-    is( $master->comment, 'add again' );
+    my $commit = $git->master;
 
-    my $tree = $git->get_object( $master->tree );
+    is( $commit->kind, 'commit' );
+    is( $commit->size, 256 );
+    like( $commit->sha1, qr/^[a-z0-9]{40}$/ );
+    is( $commit->tree, '37b4fcd62571f07408e830f455268891f95cecf5' );
+    like( $commit->parent, qr/^[a-z0-9]{40}$/ );
+    like( $commit->author,
+        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
+    like( $commit->committer,
+        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
+    is( $commit->comment, 'add again' );
+
+    my $tree = $git->get_object( $commit->tree );
     is( $tree->kind, 'tree' );
     is( $tree->size, 36 );
     my @directory_entries = $tree->directory_entries;
@@ -35,4 +37,64 @@ foreach my $directory qw(test-project test-project-packs) {
 hello world, again
 '
     );
+
+    $commit = $git->get_object( $commit->parent );
+    is( $commit->kind, 'commit' );
+    is( $commit->size, 259 );
+    like( $commit->sha1, qr/^[a-z0-9]{40}$/ );
+    is( $commit->tree, 'd0492b368b66bdabf2ac1fd8c92b39d3db916e59' );
+    like( $commit->parent, qr/^[a-z0-9]{40}$/ );
+    like( $commit->author,
+        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
+    like( $commit->committer,
+        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
+    is( $commit->comment, 'add emphasis' );
+
+    $tree = $git->get_object( $commit->tree );
+    is( $tree->kind, 'tree' );
+    is( $tree->size, 36 );
+    @directory_entries = $tree->directory_entries;
+    is( @directory_entries, 1 );
+    $directory_entry = $directory_entries[0];
+    is( $directory_entry->mode,     '100644' );
+    is( $directory_entry->filename, 'file.txt' );
+    is( $directory_entry->sha1, 'a0423896973644771497bdc03eb99d5281615b51' );
+
+    $blob = $git->get_object( $directory_entry->sha1 );
+    is( $blob->kind, 'blob' );
+    is( $blob->size, 13 );
+    is( $blob->content, 'hello world!
+'
+    );
+
+   $commit = $git->get_object( $commit->parent );
+    is( $commit->kind, 'commit' );
+    is( $commit->size, 213 );
+    like( $commit->sha1, qr/^[a-z0-9]{40}$/ );
+    is( $commit->tree, '92b8b694ffb1675e5975148e1121810081dbdffe' );
+    is( $commit->parent, undef );
+    like( $commit->author,
+        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
+    like( $commit->committer,
+        qr/^Your Name Comes Here <you\@yourdomain.example.com>/ );
+    is( $commit->comment, 'initial commit' );
+
+    $tree = $git->get_object( $commit->tree );
+    is( $tree->kind, 'tree' );
+    is( $tree->size, 36 );
+    @directory_entries = $tree->directory_entries;
+    is( @directory_entries, 1 );
+    $directory_entry = $directory_entries[0];
+    is( $directory_entry->mode,     '100644' );
+    is( $directory_entry->filename, 'file.txt' );
+    is( $directory_entry->sha1, '3b18e512dba79e4c8300dd08aeb37f8e728b8dad' );
+
+    $blob = $git->get_object( $directory_entry->sha1 );
+    is( $blob->kind, 'blob' );
+    is( $blob->size, 12 );
+    is( $blob->content, 'hello world
+'
+    );
+
+  
 }
